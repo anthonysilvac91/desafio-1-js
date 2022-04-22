8640de62d8ca28e692e1b826cda1d9173bd11256
anthonysilvac <anthony.silvac@gmail.com>
2022-04-21 21:38:47 -0400

    bucle

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let pregunta 
+
+do{
+    pregunta = parseFloat(prompt("¿Cuanto es 2+2?"))
+
+    if(pregunta != "4"){
+    
+        alert("respuesta incorrecta")
+    } else{
+        alert("respuesta correcta")
+    }
+
+
+    
+} while(pregunta != "4")
+
+
